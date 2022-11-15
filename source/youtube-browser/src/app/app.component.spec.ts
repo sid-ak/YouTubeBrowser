baf29828fc98528a299c439c121f86a63d81b07a
sid-ak <sidharthak25@gmail.com>
2022-11-15 00:29:08 -0500

    8: Removed outdated tests from the app component.

--- source/youtube-browser/src/app/app.component.spec.ts
+++ source/youtube-browser/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'youtube-browser'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('youtube-browser');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('youtube-browser app is running!');
-  });
 });
