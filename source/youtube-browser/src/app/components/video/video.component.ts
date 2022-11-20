c629da1cd35f1808be65cd15326b90b7be4cc876
sid-ak <sidharthak25@gmail.com>
2022-11-20 12:14:52 -0500

    10: Integrated the API with the video.component.

--- source/youtube-browser/src/app/components/video/video.component.ts
+++ source/youtube-browser/src/app/components/video/video.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil } from 'rxjs';
+import { YoutubeVideo } from 'src/app/models/youtube-data.model';
+import { YoutubeDataService } from 'src/app/services/youtube-data/youtube-data.service';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private readonly destroyed$ = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  public video: YoutubeVideo | null = null
+
+  constructor(
+    private readonly ytService: YoutubeDataService,
+    private readonly activatedRoute: ActivatedRoute) { }
+
+  async ngOnInit(): Promise<void> {
+    var videoId: string = "";
+
+    this.activatedRoute.queryParams.pipe(
+      takeUntil(this.destroyed$)).subscribe(
+        e => videoId = (e['id'])
+      );
+    
+    const video = await this.ytService.getVideo(videoId);
+    this.video = video;
+
+    console.log(video);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
   }
 
 }
