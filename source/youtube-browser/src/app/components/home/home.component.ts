c3a66216cf2a5ad7aea1c73408978cce7faaf77c
sid-ak <sidharthak25@gmail.com>
2022-11-20 10:17:22 -0500

    10: Handled navigation in the home component using routes.

--- source/youtube-browser/src/app/components/home/home.component.ts
+++ source/youtube-browser/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventService } from 'src/app/services/event/event.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly eventService: EventService,
+    private readonly router: Router) { }
 
+  // Triggers events to handle navigation and its data.
   ngOnInit(): void {
+    this.eventService.queryChanged$.subscribe(
+      query => this.router.navigate(
+        ["video-list"],
+        {queryParams: {query: query}})
+    );
+
+    this.eventService.videoSelected$.subscribe(
+      videoId => this.router.navigate(
+        ["video"],
+        {queryParams: {id: videoId}})
+    );
   }
 
 }
