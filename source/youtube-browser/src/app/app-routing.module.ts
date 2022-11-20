872e2b6d6b251481b771c9c29ffa83b8c6e607e7
sid-ak <sidharthak25@gmail.com>
2022-11-20 10:16:55 -0500

    10: Added routes to the routing module.

--- source/youtube-browser/src/app/app-routing.module.ts
+++ source/youtube-browser/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
+import { VideoListComponent } from './components/video-list/video-list.component';
 import { VideoComponent } from './components/video/video.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", redirectTo: "video-list", pathMatch: "full" },
+    { path: "video-list", component: VideoListComponent },
+    { path: "video", component: VideoComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
